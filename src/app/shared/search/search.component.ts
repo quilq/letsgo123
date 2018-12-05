e45a2de315f56318f8dfe6b5f73909a8d3d9098e
Qui <quilq@outlook.com>
2018-12-06 00:28:28 +0700

    Update Layout

--- src/app/shared/search/search.component.ts
+++ src/app/shared/search/search.component.ts
@@ -9,11 +9,25 @@ export class SearchComponent implements OnInit {
 
   constructor() { }
 
+  date: Date;
+
   ngOnInit() {
   }
 
   searchTour(from, to, date){
     console.log(from, to, date);
   }
+
+  departures = [
+    {value: 'Citi 11', viewValue: 'Citi 12'},
+    {value: 'Citi 21', viewValue: 'Citi 22'},
+    {value: 'Citi 31', viewValue: 'Citi 32'}
+  ];
+
+  arrivals = [
+    {value: 'Citi 41', viewValue: 'Citi 42'},
+    {value: 'Citi 51', viewValue: 'Citi 52'},
+    {value: 'Citi 61', viewValue: 'Citi 62'}
+  ];
 
 }
