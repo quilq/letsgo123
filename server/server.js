059deeca62a96d7f2b567bc2ffa9b1c2ba164d59
Qui <quilq@outlook.com>
2019-01-13 15:26:35 +0700

    update server for production

--- server/server.js
+++ server/server.js
@@ -4,14 +4,26 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT;
 const helmet = require('helmet');
+const compression = require('compression');
 
 const api = require('./routes/api');
 
 app.use(helmet());
+app.use(compression());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+//Output folder
+const publicPath = path.join(__dirname, './../dist/');
+app.use(express.static(publicPath));
+
 app.use('/api', api);
+
+//Send all other routes (define last):
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './../dist/index.html'));
+})
 
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port} !`);
